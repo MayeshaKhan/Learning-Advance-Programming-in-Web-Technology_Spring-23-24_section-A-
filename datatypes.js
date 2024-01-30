511b96964870eda83cd11e37410d98abe9e83867
MayeshaKhan <137705344+MayeshaKhan@users.noreply.github.com>
2024-01-30 13:52:44 +0600

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,18 @@
+//string
+let firstName = 'John';
+//numbers
+let cgpa = 3.82;
+//boolean
+let isOkay= true;
+//null
+let a = null;
+//undefined 
+let z;
+//NaN
+let result = 0/0;
+console.log(result);
+
+let notANumber= parseInt("Hello");
+
+console.log(notANumber);
+
---
+++ variable.js
@@ -0,0 +1,8 @@
+var name = "Maisha Khan";
+let age = 24;
+
+const PI= 3.14;
+console.log(name);
+console.log(age);
+console.log(PI);
+
