8ec6434270a9cb2e0fd59f79c19072437eed5e51
20-43829-2 <mayeshakhan2k18@gmail.com>
2024-01-30 21:34:17 +0600

    Create modules.js

---
+++ modules.js
@@ -0,0 +1,9 @@
+//named export
+export const greetings = {
+    morning: "Good Morning",
+    afternoon: "Good Afternoon",
+    evening: "Good evening",
+};
+
+const farewell = "Goodbye";
+export default farewell;
