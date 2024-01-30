f75e4492c40e190b1e70d4bd5cba0d4decf837fd
20-43829-2 <mayeshakhan2k18@gmail.com>
2024-01-30 20:39:25 +0600

    Create template_literal.js

---
+++ template_literal.js
@@ -0,0 +1,16 @@
+const name = "Alice";
+
+const greeting = 'Hello, ${name}!';
+
+console.log(greeting);
+
+//multiline string
+const multiline = 'This is a multiline string';
+console.log(multiline);
+
+//string interpolation
+const a = 5;
+const b = 10;
+
+const sum = 'The sum of ${a} and ${b} is ${a + b}';
+console.log(sum);
