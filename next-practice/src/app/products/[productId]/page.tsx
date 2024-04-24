9c2fecce27c0ce9b9792eae3db1abeff239de544
20-43829-2 <mayeshakhan2k18@gmail.com>
2024-04-24 19:54:49 +0600

    next js practice

---
+++ next-practice/src/app/products/[productId]/page.tsx
@@ -0,0 +1,4 @@
+export default function ProductDetails({params,}: {params:{productId: string};})
+{
+    return <h1>Details of the product {params.productId}</h1>
+}
---
+++ next-practice/src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -0,0 +1,9 @@
+export default function notFound()
+{
+    return (
+        <>
+        <h2>Page not found</h2>
+        <p>COuld not find requested resource</p>
+        </>
+    )
+}
